0d459ebca9ba47ca7bf9a0c6fb2fcd06d905c31c
anamariapastiu <apastiu02@gmail.com>
2024-07-30 14:08:41 +0300

    migrated tables

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Laravel Chat System Project
-
+1.composer install
+2..env:
+->copy .env example
+->make your own database and complete the .env
+->generate key: php artisan key:generate
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('id')->primary();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('name');
+            $table->string('avatar')->nullable();
+            $table->boolean('status')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
