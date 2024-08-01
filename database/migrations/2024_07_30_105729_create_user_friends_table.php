c8276bedeaafbfe65ab85e0acec51c95ed483b57
Popa Alexandru Vasile <popavasile02@yahoo.com>
2024-08-01 10:02:46 +0300

    Modified Migration tables from oauth to enable clients and users to have uuid, also modifie web.php to test it,you will need to drop&regenerate your client personal keys

--- database/migrations/2024_07_30_105729_create_user_friends_table.php
+++ database/migrations/2024_07_30_105729_create_user_friends_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('user_friends', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->integer('friend_id');
+            $table->foreignUuid('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignUuid('friend_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
