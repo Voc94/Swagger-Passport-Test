de3e11e19455c08a729c448c8145d15ead3d0a27
voc94 <popavasile02@yahoo.com>
2024-08-04 21:05:45 +0300

    Swagger & Passport Fix, removed duplicate migrations,added swagger annotations&middleware config in bootstrap\app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
         $middleware->alias([
             'role'=> RoleMiddleware::class,
+        ])
+        ->validateCsrfTokens([
+            'api/*'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
